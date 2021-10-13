71740c5de8c6c9c3a6f436ab5552a9da913e7209
harunfr <harun-626@hotmail.com>
2021-10-13 20:05:36 +0300

    hidden search widget's basic features completed

--- hidden_search_widget/script.js
+++ hidden_search_widget/script.js
@@ -1,44 +1,8 @@
 "use strict";
 
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".form");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+searchInput.addEventListener("click", (e) => {
+  form.classList.add('ready');
+});
