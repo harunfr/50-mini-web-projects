a2c518cf60945f5f21e19070ff53e4847afbb206
harunfr <harun-626@hotmail.com>
2021-10-13 12:06:50 +0300

    small fixes

--- progress_steps_2/script.js
+++ progress_steps_2/script.js
@@ -1,8 +1,7 @@
+"use strict";
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
-
-const steps = [...document.querySelectorAll(".step")];
-console.log(steps);
+const steps = document.querySelectorAll(".step");
 const progressLine = document.querySelector(".progress-line");
 
 prevButton.addEventListener("click", handleProgress);
@@ -16,12 +15,23 @@ function handleProgress(e) {
   } else if (e.target.textContent == "Previous" && stepCount > 1) {
     stepCount--;
   }
+
   progressLine.style.width = `${(stepCount - 1) * 33.3}%`;
+
   steps.forEach((step, index) => {
-    if (index < stepCount) {
-      step.style.borderColor = "rgb(48, 192, 228)"
+    if (index < stepCount ) {
+      step.style.borderColor = "rgb(48, 192, 228)";
     } else {
-      step.style.borderColor = "#bbb"
+      step.style.borderColor = "#bbb";
     }
   });
+  
+  if (stepCount == 1) {
+    prevButton.disabled = true;
+  } else if (stepCount == 4) {
+    nextButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+    nextButton.disabled = false;
+  }
 }
