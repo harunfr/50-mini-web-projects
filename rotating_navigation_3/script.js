81666edacd6b69f236be453665749eaff7d7e4ec
harunfr <harun-626@hotmail.com>
2021-10-13 18:32:52 +0300

    need refactor for translate property, general layout done

--- rotating_navigation_3/script.js
+++ rotating_navigation_3/script.js
@@ -1,44 +1,18 @@
 "use strict";
+const openBtn = document.querySelector(".open");
+const closeBtn = document.querySelector(".close");
+const contentWrapper = document.querySelector(".content-wrapper");
+const buttonWrapper = document.querySelector(".open-and-close");
 
+openBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
 
+function openNav() {
+  contentWrapper.classList.add("rotated");
+  buttonWrapper.classList.add("active");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function closeNav() {
+  contentWrapper.classList.remove("rotated");
+  buttonWrapper.classList.remove("active");
+}
